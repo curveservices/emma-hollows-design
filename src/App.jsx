4935b955837f516ffd3fdfc9544a4298576aa5f0
Paul <p.rossiter83@gmail.noreply.github.com>
2025-03-03 14:54:15 +0000

    Home and about pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
-import React, { Suspense } from 'react';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-const Home = React.lazy(() => import('./pages/home/index.jsx'));
-import Layout from './layout/main/index.jsx';
-import './App.scss'
-
+import React, { Suspense } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+const Home = React.lazy(() => import("./pages/home/index.jsx"));
+import LoadingSpinner from './components/loadingSpinner/index.jsx';
+import Layout from "./layout/main/index.jsx";
+import "./App.scss";
+import About from "./pages/about/index.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Suspense fallback={<div>Loading...</div>}><Home /></Suspense>} />
+      <Route path="/" element={<Layout />}>
+        <Route
+          index
+          element={
+            <Suspense fallback={<LoadingSpinner/>}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route path="about-me" element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <About />
+          </Suspense>
+        }
+        />
       </Route>
-    </>
+
+    </>,
   ),
 );
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;
