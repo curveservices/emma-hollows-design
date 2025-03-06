5de0fe26b4d6819984b66c8c1debc2a69e806146
Paul <p.rossiter83@gmail.noreply.github.com>
2025-03-06 13:09:54 +0000

    cardWidth prop added to change width dynamically styling change to adjust to this

--- src/components/cards/flipcard/index.jsx
+++ src/components/cards/flipcard/index.jsx
@@ -2,39 +2,42 @@ import React from 'react';
 import Button from '../../button';
 import './index.scss';
 
-const FlipCard = ({ frontImage, backImage, title, description, viewLink }) => (
-        <div className="flip-card">
+const FlipCard = ({ frontImage, backImage, title, description, viewLink, cardWidth }) => {
+    const cardStyle = cardWidth ? { '--card-width': cardWidth } : {};
+    return (
+        <div className="flip-card" style={cardStyle}>
             <div className="flip-card-inner">
                 <div className="flip-card-front">
                     <img
-                    src={frontImage}
-                    alt={title}
-                    loading="lazy"
-                    className="card-image"
+                        src={frontImage}
+                        alt={title}
+                        loading="lazy"
+                        className="card-image"
                     />
                     <div className="front-title">
-                    <h3 className="name">{title}</h3>
+                        <h3 className="name">{title}</h3>
                     </div>
                 </div>
-            <div className="flip-card-back">
-                <img
-                src={backImage}
-                alt={title}
-                loading="lazy"
-                className="card-back-image"
-                />
-                <h3 className="name">{title}</h3>
-                <p className="desc subtitle">{description}</p>
-                <div className="btn-container">
-                    <Button
-                        text="View"
-                        link={viewLink}
-                        background="var(--secondary)"
+                <div className="flip-card-back">
+                    <img
+                        src={backImage}
+                        alt={title}
+                        loading="lazy"
+                        className="card-back-image"
                     />
+                    <h2 className="name">{title}</h2>
+                    <p className="desc subtitle">{description}</p>
+                    <div className="btn-container">
+                        <Button
+                            text="View"
+                            link={viewLink}
+                            background="var(--secondary)"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    )       
+};
 
 export default FlipCard;
