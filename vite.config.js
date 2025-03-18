327bb7e2cd709eeac021214f134935d7da9aa947
Paul <p.rossiter83@gmail.noreply.github.com>
2025-03-18 17:30:42 +0000

    Enhance Vite configuration with compression and PurgeCSS plugins for optimized build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,75 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import compression from "vite-plugin-compression";
+import purgecss from "vite-plugin-purgecss";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/",
+  css: {
+    preprocessorOptions: {
+      sass: {
+        api: "modern",
+        silenceDeprecations: ["legacy-js-api"],
+      },
+    },
+    modules: {
+      localsConvention: "camelCase", // makes sure the classes are camelCase, like priceCard
+    },
+  },
+  plugins: [
+    react(),
+    purgecss({
+      content: [
+        "./index.html",
+        "./src/**/*.{jsx,js,tsx,ts}",
+        "./public/**/*.html",
+      ],
+      safelist: {
+        standard: [
+          /^toast-.*$/,
+          "ReactToastify__toast",
+          "ReactToastify__toast-container",
+          "ReactToastify__toast-body",
+          "ReactToastify__progress-bar",
+          "load-spinner",
+          /^slick-/,
+          "slick-list",
+          "slick-track",
+          "slick-slide",
+          "slick-active",
+          "slick-current",
+          "slick-prev",
+          "slick-next",
+        ],
+      },
+    }),
+    compression({
+      algorithm: "gzip",
+      ext: ".gz",
+      threshold: 10240,
+      filter: /\.(js|mjs|json|css|html|svg|ico|png|jpg|webp|woff|woff2)$/,
+    }),
+    compression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+      threshold: 10240,
+      filter: /\.(js|mjs|json|css|html|svg|ico|png|jpg|webp|woff|woff2)$/,
+    }),
+  ],
+  build: {
+    minify: "esbuild",
+    modulePreload: true,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name].[hash][extname]",
+        chunkFileNames: "[name].[hash].js",
+        entryFileNames: "[name].[hash].js",
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+  define: { "process.env": process.env },
 });
