fbe334cc91390aae4b5b28db23e3aff12caf4067
Paul <p.rossiter83@gmail.noreply.github.com>
2025-03-25 15:13:34 +0000

    Implement dropdown menu in navbar for portfolio options and enhance styles for better visibility

--- src/layout/navbar/index.jsx
+++ src/layout/navbar/index.jsx
@@ -1,13 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import {
-  faFileAlt,
-  faHouse,
-  faSuitcase,
-  faUserAlt,
-} from "@fortawesome/free-solid-svg-icons";
+import { faFileAlt, faHouse, faSuitcase, faUserAlt } from "@fortawesome/free-solid-svg-icons";
 
 import Button from "../../components/button";
 import logo from "../../assets/images/logo1.png";
@@ -17,6 +11,7 @@ import MenuToggle from "../../components/menuToggle";
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
   const [showNav, setShowNav] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -31,76 +26,173 @@ const Navbar = () => {
       <header className={scrolled ? "navbar-scroll" : ""}>
         <NavLink to="/" aria-label="home page">
           <div className="title-container">
-            <img
-              src={logo}
-              alt="London theatre Costume design, wardrobe, maker and film"
-              className="mobile-logo"
-            />
+            <img src={logo} alt="London theatre Costume design, wardrobe, maker and film" className="mobile-logo" />
           </div>
-          <img
-            src={logo}
-            alt="London theatre Costume design, wardrobe, maker and film"
-            className="logo"
-          />
+          <img src={logo} alt="London theatre Costume design, wardrobe, maker and film" className="logo" />
         </NavLink>
         <nav className={showNav ? "mobile-show" : ""}>
-          <NavLink
-            to="/"
-            title="Home Page"
-            className={({ isActive }) => isActive ? 'active' : ''}
-            onClick={() => setShowNav(false)}
-          >
+          <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''} onClick={() => setShowNav(false)}>
             <div className="menu-container">
               <FontAwesomeIcon icon={faHouse} className="mobile-icon" />
               <div className="text">Home</div>
             </div>
           </NavLink>
-          <NavLink
-            to="about-me"
-            title="About Me"
-            className={({ isActive }) => isActive ? 'active' : ''}
-            onClick={() => setShowNav(false)}
-          >
+          <NavLink to="/about-me" className={({ isActive }) => isActive ? 'active' : ''} onClick={() => setShowNav(false)}>
             <div className="menu-container">
               <FontAwesomeIcon icon={faUserAlt} className="mobile-icon" />
               <div className="text">About Me</div>
             </div>
           </NavLink>
-          <NavLink
-            to="/portfolio"
-            title="Portfolio"
-            className={({ isActive }) => isActive ? 'active' : ''}
-            onClick={() => setShowNav(false)}
+          <div 
+            className="dropdown" 
+            onMouseEnter={() => setShowDropdown(true)} 
+            onMouseLeave={() => setShowDropdown(false)}
           >
-            <div className="menu-container">
-              <FontAwesomeIcon icon={faSuitcase} className="mobile-icon" />
-              <div className="text">Portfolio</div>
+            <NavLink to="/portfolio" className={({ isActive }) => isActive ? 'active' : ''} onClick={() => setShowNav(false)}>
+              <div className="menu-container">
+                <FontAwesomeIcon icon={faSuitcase} className="mobile-icon" />
+                <div className="text">Portfolio</div>
             </div>
-          </NavLink>
-          <NavLink
-            to="/cv"
-            title="CV"
-            className={({ isActive }) => isActive ? 'active' : ''}
-            onClick={() => setShowNav(false)}
-          >
+            </NavLink>
+            {showDropdown && (
+              <div className="dropdown-menu">
+                <NavLink to="/designer" onClick={() => setShowDropdown(false)} className="dropdown-item">Designer</NavLink>
+                <NavLink to="/maker" onClick={() => setShowDropdown(false)} className="dropdown-item">Maker</NavLink>
+                <NavLink to="/wardrobe" onClick={() => setShowDropdown(false)} className="dropdown-item">Wardrobe</NavLink>
+                <NavLink to="/film" onClick={() => setShowDropdown(false)} className="dropdown-item">Film</NavLink>
+              </div>
+            )}
+          </div>
+          <NavLink to="/cv" className={({ isActive }) => isActive ? 'active' : ''} onClick={() => setShowNav(false)}>
             <div className="menu-container">
               <FontAwesomeIcon icon={faFileAlt} className="mobile-icon" />
               <div className="text">CV</div>
             </div>
           </NavLink>
-          <Button
-            text="Contact Me"
-            link="/contact"
-            background="var(--secondary)"
-            color="#000"
-            boxShadow="none"
-            onClick={() => setShowNav(false)}
-          />
+          <Button text="Contact Me" link="/contact" background="var(--secondary)" color="#000" boxShadow="none" onClick={() => setShowNav(false)} />
         </nav>
-         <MenuToggle showNav={showNav} setShowNav={setShowNav} />
+        <MenuToggle showNav={showNav} setShowNav={setShowNav} />
       </header>
     </>
   );
 };
 
 export default Navbar;
+
+// import React, { useEffect, useState } from "react";
+// import { NavLink } from "react-router-dom";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// import {
+//   faFileAlt,
+//   faHouse,
+//   faSuitcase,
+//   faUserAlt,
+// } from "@fortawesome/free-solid-svg-icons";
+
+// import Button from "../../components/button";
+// import logo from "../../assets/images/logo1.png";
+// import "./index.scss";
+// import MenuToggle from "../../components/menuToggle";
+
+// const Navbar = () => {
+//   const [scrolled, setScrolled] = useState(false);
+//   const [showNav, setShowNav] = useState(false);
+//   const [showDropdown, setShowDropdown] = useState(false);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const scrollY = window.scrollY;
+//       setScrolled(scrollY > 100);
+//     };
+//     window.addEventListener("scroll", handleScroll);
+//   }, []);
+
+//   return (
+//     <>
+//       <header className={scrolled ? "navbar-scroll" : ""}>
+//         <NavLink to="/" aria-label="home page">
+//           <div className="title-container">
+//             <img
+//               src={logo}
+//               alt="London theatre Costume design, wardrobe, maker and film"
+//               className="mobile-logo"
+//             />
+//           </div>
+//           <img
+//             src={logo}
+//             alt="London theatre Costume design, wardrobe, maker and film"
+//             className="logo"
+//           />
+//         </NavLink>
+//         <nav className={showNav ? "mobile-show" : ""}>
+//           <NavLink
+//             to="/"
+//             title="Home Page"
+//             className={({ isActive }) => isActive ? 'active' : ''}
+//             onClick={() => setShowNav(false)}
+//           >
+//             <div className="menu-container">
+//               <FontAwesomeIcon icon={faHouse} className="mobile-icon" />
+//               <div className="text">Home</div>
+//             </div>
+//           </NavLink>
+//           <NavLink
+//             to="about-me"
+//             title="About Me"
+//             className={({ isActive }) => isActive ? 'active' : ''}
+//             onClick={() => setShowNav(false)}
+//           >
+//             <div className="menu-container">
+//               <FontAwesomeIcon icon={faUserAlt} className="mobile-icon" />
+//               <div className="text">About Me</div>
+//             </div>
+//           </NavLink>
+//           <div className="portfolio-wrapper" onMouseEnter={() => setShowDropdown(true)}>
+//             <NavLink
+//             to="/portfolio"
+//             title="Portfolio"
+//             className={({ isActive }) => isActive ? 'active' : ''}
+//             onClick={() => setShowNav(false)}
+//           >
+//             <div className="menu-container">
+//               <FontAwesomeIcon icon={faSuitcase} className="mobile-icon" />
+//               <div className="text">Portfolio</div>
+//             </div>
+//             </NavLink>
+//             {showDropdown && (
+//                <div className="dropdown">
+//               <NavLink to="/designer" className="dropdown-item">Designer</NavLink>
+//               <NavLink to="/maker" className="dropdown-item">Maker</NavLink>
+//               <NavLink to="/wardrobe" className="dropdown-item">Wardrobe</NavLink>
+//               <NavLink to="/film" className="dropdown-item">Film</NavLink>
+//               </div>
+//             )}
+//           </div>
+//           <NavLink
+//             to="/cv"
+//             title="CV"
+//             className={({ isActive }) => isActive ? 'active' : ''}
+//             onClick={() => setShowNav(false)}
+//           >
+//             <div className="menu-container">
+//               <FontAwesomeIcon icon={faFileAlt} className="mobile-icon" />
+//               <div className="text">CV</div>
+//             </div>
+//           </NavLink>
+//           <Button
+//             text="Contact Me"
+//             link="/contact"
+//             background="var(--secondary)"
+//             color="#000"
+//             boxShadow="none"
+//             onClick={() => setShowNav(false)}
+//           />
+//         </nav>
+//          <MenuToggle showNav={showNav} setShowNav={setShowNav} />
+//       </header>
+//     </>
+//   );
+// };
+
+// export default Navbar;
