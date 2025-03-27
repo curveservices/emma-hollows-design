8b45ed365787f4bafd2d717295bc86ceae83090e
Paul <p.rossiter83@gmail.noreply.github.com>
2025-03-27 18:17:23 +0000

    Refactor 404 page to use Helmet for SEO and update styles for improved readability

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,21 +1,19 @@
 import "./index.scss";
 import notFound from "../../assets/images/404.webp";
 import Button from "../../components/button";
-// import { Helmet, HelmetProvider } from "react-helmet-async";
+import Helmet from "../../components/helmet";
 
 const NotFound = () => {
   return (
     <>
       <div className="container">
         <h1>PAGE NOT FOUND</h1>
-        <Button text="Back home" link="/" color='#fff'/>
+        <Button text="Back home" link="/" background='var(--secondary)'/>
         <img src={notFound} alt="page not found" />
       </div>
-      {/* <HelmetProvider>
-        <Helmet>
-          <title>Error 404 - Page not found</title>
-        </Helmet>
-      </HelmetProvider> */}
+      <Helmet
+          title="Error 404 Page Not Found"
+        />
     </>
   );
 };
