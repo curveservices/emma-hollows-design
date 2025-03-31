25becca8a793babbb7faa6e8369bdf26bc6a616e
Paul <p.rossiter83@gmail.noreply.github.com>
2025-03-31 17:32:19 +0100

    Refactor BooksContainer and FlipBook components to use FlipPage for improved functionality and remove unused code

--- src/components/sketchBooks/flipBook/index.jsx
+++ src/components/sketchBooks/flipBook/index.jsx
@@ -1,57 +1,54 @@
-import React from 'react';
-import HTMLFlipBook from 'react-pageflip';
-import coverImage from '../../../assets/images/enchanted-cover.jpg';
-import page1Image from '../../../assets/images/enchanted1.avif';
-import page2Image from '../../../assets/images/enchanted2.avif';
-import page3Image from '../../../assets/images/enchanted3.avif';
-import './index.scss';
-
-const BookFlipSmall = ({ title }) => {
+import React, { useEffect, useState } from "react";
+import FlipPage from "react-flip-page";
+import { db } from "../../../../firebase.config"; // Adjust path to your Firebase config
+import { collection, getDocs } from "firebase/firestore";
+import "./index.scss";
+const FlipBook = ({ images, title }) => {
   return (
-    <div className='bookflip-container'>
-       <div className="bookflip-small">
-      <HTMLFlipBook width={200} height={300} showCover={true}>
-        {/* Cover Page */}
-        <div className="page page-cover">
-          <img src={coverImage} alt="Book Cover" />
-          <h2 className="page-title">{title}</h2>
-        </div>
-        {/* Page 1 */}
-        <div className="page">
-          <img src={page1Image} alt="Page 1" />
-        </div>
-        {/* Page 2 */}
-        <div className="page">
-          <img src={page2Image} alt="Page 2" />
-        </div>
-        <div className="page">
-          <img src={page3Image} alt="Page 2" />
-        </div>
-      </HTMLFlipBook>
-      </div>
-      <div className="bookflip-small">
-      <HTMLFlipBook width={200} height={300} showCover={true}>
-        {/* Cover Page */}
-        <div className="page page-cover">
-          <img src={coverImage} alt="Book Cover" />
-          <h2 className="page-title">{title}</h2>
-        </div>
-        {/* Page 1 */}
-        <div className="page">
-          <img src={page1Image} alt="Page 1" />
-        </div>
-        {/* Page 2 */}
-        <div className="page">
-          <img src={page2Image} alt="Page 2" />
-        </div>
-        <div className="page">
-          <img src={page3Image} alt="Page 2" />
-        </div>
-      </HTMLFlipBook>
+    <div className="flipbook-container">
+      <FlipPage
+        orientation="horizontal"
+        showTouchHint
+        flipOnTouch
+        showHint
+        flipOnTouchZone="20"
+        perspective="30em"
+      >
+        {images.map((url, index) => (
+          <article key={index} className="flip-page">
+            <img src={url} alt={`Page ${index + 1}`} className="flip-image" />
+            <div className="title">{title}</div>
+          </article>
+        ))}
+      </FlipPage>
     </div>
-    </div>
-   
   );
 };
 
-export default BookFlipSmall;
+const FlipBookGallery = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const booksCollection = collection(db, "sketchbooks");
+      const booksSnapshot = await getDocs(booksCollection);
+      
+      const bookData = booksSnapshot.docs.map(doc => doc.data().images);
+      setBooks(bookData);
+    };
+
+    fetchImages();
+  }, []);
+
+  return (
+    <div className="gallery-container">
+      {books.map(( images, index) => (
+           <FlipBook key={index} images={images} />
+      ))}
+    </div>
+  );
+};
+
+export default FlipBookGallery;
+
+
