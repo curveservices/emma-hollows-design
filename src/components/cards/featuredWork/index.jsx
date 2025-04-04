6176e0a91c864e55c8a744cddb2167f7ca037daa
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-04 16:49:21 +0100

    Remove unused image imports and default card data from FeaturedWork component

--- src/components/cards/featuredWork/index.jsx
+++ src/components/cards/featuredWork/index.jsx
@@ -1,36 +1,5 @@
 import React from 'react';
 import FlipCard from '../flipcard';
-import bodice from '../../../assets/images/bodice.webp';
-import card1back from '../../../assets/images/bodice1.webp';
-import tina from '../../../assets/images/home-hero.webp';
-import card2back from '../../../assets/images/tina2.webp';
-import sophie from '../../../assets/images/sophie1.webp';
-import card3back from '../../../assets/images/heineken3.webp';
-
-
-const defaultCardsData = [
-  {
-    frontImage: bodice,
-    backImage: card1back,
-    title: 'Magic at the Musicals',
-    description: 'Designer and Maker',
-    viewLink: 'magic-at-the-musicals'
-  },
-  {
-    frontImage: tina,
-    backImage: card2back,
-    title: 'Tina the musical',
-    description: 'Principal Dresser to Tina',
-    viewLink: 'tina-turner-musical'
-  },
-  {
-    frontImage: sophie,
-    backImage: card3back,
-    title: 'Heineken Jackets and Hats',
-    description: 'Designer and Maker',
-    viewLink: 'heineken-jackets'
-  },
-];
 
 const FeaturedWork = ({ cardsData = defaultCardsData, cardWidth }) => {
   return (
