29d1811203159698f37a943afc0ff234d76f5515
Paul <p.rossiter83@gmail.noreply.github.com>
2025-06-25 20:42:48 +0100

    Refactor Illustrator page layout: remove hero image, add Socials component, and clean up structure

--- src/pages/illustrator/index.jsx
+++ src/pages/illustrator/index.jsx
@@ -1,14 +1,11 @@
-import portfolioHero from '../../assets/images/jekyllandhyde.webp';
-import Button from '../../components/button';
+import Socials from '../../components/socials';
 import FeaturedWork from '../../components/cards/featuredWork'
-import useScrollStates from '../../components/scrollState';
 import Helmet from '../../components/helmet';
 import LoadingSpinner from '../../components/loadingSpinner';
 import useFirestoreData from '../../components/useFirestoreData';
 import './index.scss';
 
 const Illustrator = () => {
-  const { second } = useScrollStates();
   const { cardsData, loading, error } = useFirestoreData('illustrations');
   if (loading) return <LoadingSpinner />;
   if (error) return <div className='error-message'>Error: {error}</div>
@@ -16,27 +13,16 @@ const Illustrator = () => {
     <>
       <div className='portfolio-page'>
         <section className="hero">
-          <img
-            src={portfolioHero}
-            alt='Emma Hollows, custome design, west end London'
-            className='hero-image'
-          />
           <div className="text-box">
             <h1 className="main-title">Illustrator</h1>
             <div className="subtitle">Emma Hollows</div>
           </div>
-        </section>
-        <section className={`second-section ${second ? 'anim' : 'none'}`}>
-          <div className="text-box">
-            <div className="subtitle">Illustrations</div>
-            <h2>Explore my Illustrations</h2>
-            <p>
-              I have been working for a number of years in both the UK and the USA where I trained in Costume Design at the University of
-              Massachusetts Amherst. Some previous highlights of my career have been working on The Nutcracker at the Royal Albert Hall in London;
-              numerous productions at Shakespeare and Company in Massachusetts; and touring up the East Coast of the USA with The Cambridge American Stage Tour.
-            </p>
+          <div className="socials">
+            <Socials />
           </div>
-          <FeaturedWork cardsData={cardsData} cardWidth='300px' />
+        </section>
+        <section className={`second-section`}>
+          <FeaturedWork cardsData={cardsData} cardWidth='300px' cardHeight='380px' />
         </section>
          <Helmet
           title="Portfolio | London Costume Desgin for Theatre and Film"
