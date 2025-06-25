c1d363b831963714e8402eac56fe0fa48f3b58df
Paul <p.rossiter83@gmail.noreply.github.com>
2025-06-25 20:42:59 +0100

    Refactor Maker page layout: remove unused imports, update hero section to include Socials component, and clean up structure

--- src/pages/maker/index.jsx
+++ src/pages/maker/index.jsx
@@ -1,14 +1,10 @@
-import useScrollStates from '../../components/scrollState';
-import maker from '../../assets/images/bodice.webp';
-import Button from '../../components/button';
+import Socials from '../../components/socials';
 import FeaturedWork from '../../components/cards/featuredWork';
 import LoadingSpinner from '../../components/loadingSpinner';
 import useFirestoreData from '../../components/useFirestoreData';
 import Helmet from '../../components/helmet';
 
 const Maker = () => {
-    const { second, third } = useScrollStates();
-    
     const { cardsData: makerData, loading: loadingMaker, error: makerError } = useFirestoreData('maker');
     const { cardsData: millineryData, loading: loadingMillinery, error: millineryError } = useFirestoreData('millinery');
     const { cardsData: masksData, loading: loadingMasks, error: masksError } = useFirestoreData('masks');
@@ -16,49 +12,42 @@ const Maker = () => {
     if (loadingMaker || loadingMillinery || loadingMasks || loadingOther) return <LoadingSpinner />;
     if (makerError || millineryError || masksError || otherError) return <div className='error-message'>Error: {makerError || millineryError || masksError || otherError}</div>;
   return (
-    <div className='maker-page'>
-        <section className="hero">
-            <img
-                src={maker}
-                alt="Emma Hollows Design, maker, west-end London"
-                className='hero-image'
-              />
-              <div className="text-box">
-                  <h1 className="main-title">Maker</h1>
-                  <div className="subtitle">Emma Hollows</div>
-              </div>
-          </section>
-          <section className={`second-section ${second ? 'anim' : 'none'}`}>
-              <div className="text-box">
-                  <div className="subtitle">Maker</div>
-                  <h2>My Maker Work</h2>
-                  <p>
-                      I have been working for a number of years in both the UK and the USA where I trained in Costume Design
-                      at the University of Massachusetts Amherst. Some previous highlights of my career have been working on
-                      The Nutcracker at the Royal Albert Hall in London; numerous productions at Shakespeare and Company in Massachusetts.
-                  </p>
-              </div>
-              <FeaturedWork cardWidth='400px' cardsData={makerData} />
-              <div className="text-box">
-                  <div className="subtitle">Millinery</div>
-              </div>
-              <FeaturedWork cardWidth='400px' cardsData={millineryData} />
-              <div className="text-box">
-                  <div className="subtitle">Masks</div>
-              </div>
-              <FeaturedWork cardWidth='300px' cardsData={masksData} />
-              <div className="text-box">
-                  <div className="subtitle">Other</div>
-              </div>
-              <FeaturedWork cardWidth='400px' cardsData={otherData} />
-          </section>
-           <Helmet
-          title="Maker | London Costume Desgin for Theatre and Film"
-          description="Emma Hollows Desgin, I am a costume and set designer and maker currently working on London's West End"
-          keywords="London's West-end, theatre, costume designer, costume maker, set designer, London, theatre"
+    <div className="maker-page">
+      <section className="hero">
+        <div className="text-box">
+          <h1 className="main-title">Maker</h1>
+          <div className="subtitle">Emma Hollows</div>
+        </div>
+        <div className="socials">
+          <Socials />
+        </div>
+      </section>
+      <section className={`second-section`}>
+        <FeaturedWork
+          cardsData={makerData}
+          cardWidth="380px"
+          cardHeight="460px"
         />
+        <div className="text-box">
+          <div className="subtitle">Millinery</div>
+        </div>
+        <FeaturedWork cardsData={millineryData} />
+        <div className="text-box">
+          <div className="subtitle">Masks</div>
+        </div>
+        <FeaturedWork cardsData={masksData} cardHeight='400px'/>
+        <div className="text-box">
+          <div className="subtitle">Other</div>
+        </div>
+        <FeaturedWork cardsData={otherData} />
+      </section>
+      <Helmet
+        title="Maker | London Costume Desgin for Theatre and Film"
+        description="Emma Hollows Desgin, I am a costume and set designer and maker currently working on London's West End"
+        keywords="London's West-end, theatre, costume designer, costume maker, set designer, London, theatre"
+      />
     </div>
-  )
+  );
 }
 
 export default Maker
