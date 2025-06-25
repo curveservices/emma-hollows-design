29878ade91df59235ea4dbc71a06a66f641ed585
Paul <p.rossiter83@gmail.noreply.github.com>
2025-06-25 20:42:11 +0100

    Refactor Designer page: remove unused imports, add Socials component to hero section, and clean up layout

--- src/pages/designer/index.jsx
+++ src/pages/designer/index.jsx
@@ -1,13 +1,10 @@
-import useScrollStates from '../../components/scrollState';
-import designer from '../../assets/images/designer.webp';
-import Button from '../../components/button';
-import FeaturedWork from '../../components/cards/featuredWork';
+;import FeaturedWork from '../../components/cards/featuredWork';
 import useFirestoreData from '../../components/useFirestoreData';
 import LoadingSpinner from '../../components/loadingSpinner';
 import Helmet from '../../components/helmet';
+import Socials from '../../components/socials';
 
 const Designer = () => {
-  const { second, third } = useScrollStates();
   const {cardsData, loading, error} = useFirestoreData('designer');
   if (loading) return <LoadingSpinner />;
   if (error) return <div className='error-message'>Error: {error}</div>;
@@ -15,27 +12,15 @@ const Designer = () => {
   return (
     <div className='designer-page'>
       <section className="hero">
-        <img
-          src={designer}
-          alt="Emma Hollows Design, designer, west-end London"
-          className='hero-image'
-        />
         <div className="text-box">
           <h1 className="main-title">Designer</h1>
           <div className="subtitle">Emma Hollows</div>
         </div>
-      </section>
-      <section className={`second-section ${second ? 'anim' : 'none'}`}>
-        <div className="text-box">
-          <div className="subtitle">Costume Design</div>
-          <h2>My Design Work</h2>
-          <p>
-            As a designer I am very research driven: establishing a solid foundation of research helps me to
-            create a rich and dynamic world on stage or screen. I love to share this research with the other
-            members of my design team and the actors, to bounce ideas around and hear what discoveries they have
-            made in their own research, which all informs my costume designs.
-          </p>
+        <div className="socials">
+          <Socials />
         </div>
+      </section>
+      <section className={`second-section`}>
         <FeaturedWork cardWidth='300px' cardsData={cardsData} />
       </section>
        <Helmet
