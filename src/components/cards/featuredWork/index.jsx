edf3c13968aac448892ce228fa96e10878a05910
Paul <p.rossiter83@gmail.noreply.github.com>
2025-06-25 20:40:20 +0100

    Add cardHeight prop to FeaturedWork component for improved layout

--- src/components/cards/featuredWork/index.jsx
+++ src/components/cards/featuredWork/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import FlipCard from '../flipcard';
 
-const FeaturedWork = ({ cardsData = defaultCardsData, cardWidth }) => {
+const FeaturedWork = ({ cardsData = defaultCardsData, cardWidth, cardHeight }) => {
   return (
     <div className="card-container">
       {cardsData.map((card, index) => (
-        <FlipCard key={index} {...card} cardWidth={cardWidth} />
+        <FlipCard key={index} {...card} cardWidth={cardWidth} cardHeight={cardHeight}/>
       ))}
     </div>
   );
